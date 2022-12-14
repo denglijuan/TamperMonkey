e491386e03ec982d349590aa6299a459c7385694
邓丽娟 <denglijuan@qianxin.com>
2022-12-14 23:31:03 +0800

    fix: 解决编辑状态也出现 checkbox 元素

--- table-copy-jira.user.js
+++ table-copy-jira.user.js
@@ -2,7 +2,7 @@
 // @name:zh-CN   复制表格数据
 // @name         Table-Copy
 // @namespace    https://github.com/denglijuan/TamperMonkey
-// @version      1.3.0
+// @version      1.3.1
 // @description:zh-CN 选择表格每列数据合并并复制到粘贴板上
 // @description  Select the data in each column of the table to merge and copy to the pasteboard
 // @author       Denglijuan
@@ -19,9 +19,20 @@
   let tabelElement = null;
   let isCopyLeave = true;
   let isTheadLeave = true;
+  let timer = null;
 
+  // 复制到粘贴板
   function copy(data) {
     GM_setClipboard(data);
+
+    // 原生写法
+    // const textarea = document.createElement('textarea');
+    // document.body.appendChild(textarea);
+    // textarea.value = data;
+    // textarea.setAttribute('style', 'display:node');
+    // textarea.select();
+    // document.execCommand('copy');
+    // document.body.removeChild(textarea);
   }
 
   // 处理数据
@@ -39,12 +50,11 @@
     copy(newData);
   }
 
-  // 获取 tbody
+  // 获取 tbody 元素，获取元素文本
   function handlerTbody() {
     const tbody = tabelElement.querySelector('tbody');
     const trs = tbody.childNodes;
     const trsLen = trs.length;
-    const colsLen = cols.length;
     const max = Math.max(...cols);
     const data = new Array(trsLen)
       .fill(0)
@@ -99,57 +109,38 @@
     return true;
   }
 
-  function getEvent(event) {
-    return event ? event : window.event;
-  }
-
-  function getTarget(event) {
-    return event.target || event.srcElement;
-  }
-
-  function addHandler(element, type, handler) {
-    if (element.addEventListener) {
-      element.addEventListener(type, handler, false);
-    } else if (element.attachEvent) {
-      element.attachEvent('on' + type, handler);
-    } else {
-      element['on' + type] = handler;
-    }
-  }
-
   function handlerCopyIcon(event) {
-    const e = event ? event : window.event;
-    const target = event.target || event.srcElement;
-    let svg = target;
+    const target = event.target;
+    let svgElement = target;
 
-    if (svg.nodeName === 'path') {
-      svg = svg.parentElement;
+    if (svgElement.nodeName === 'path') {
+      svgElement = svgElement.parentElement;
     }
 
-    const input = svg.previousElementSibling;
+    const input = svgElement.previousElementSibling;
     const result = validatorCloumns(input.value);
     result && handlerTbody();
   }
 
   // 给图标创建事件
   function createIconEventListener() {
-    const copyIcon = document.querySelector('.copy-icon');
-    addHandler(copyIcon, 'click', handlerCopyIcon);
+    const copyIcon = document.querySelector('.d-copy-table__icon');
+    copyIcon.addEventListener('click', handlerCopyIcon);
   }
 
   // 展示复制列元素
   function showCopyElement(top, left) {
-    const copy = document.querySelector('.table-wrap__copy');
+    const copy = document.querySelector('.d-copy-table__copy');
     copy.style.display = 'block';
 
     // 减去元素高度
-    copy.style.top = Math.max(top - 32, 8) + 'px';
-    copy.style.left = left - 128 + 'px';
+    copy.style.top = top - 35 + 'px';
+    copy.style.left = left - 134 + 'px';
   }
 
   // 隐藏复制列元素
   function hideCopyElement() {
-    const copy = document.querySelector('.table-wrap__copy');
+    const copy = document.querySelector('.d-copy-table__copy');
     copy.style.display = 'none';
   }
 
@@ -170,8 +161,7 @@
 
   // 处理表头移入事件
   function handlerTheadMouseenter(event) {
-    const e = getEvent(event);
-    const target = getTarget(e);
+    const target = event.target;
     const position = target.getBoundingClientRect();
     const top = position.top;
     const left = position.right;
@@ -184,8 +174,8 @@
   function createTheadEventListener() {
     const theads = document.querySelectorAll('thead');
     for (let i = 0; i < theads.length; i++) {
-      addHandler(theads[i], 'mouseenter', handlerTheadMouseenter);
-      addHandler(theads[i], 'mouseleave', handlerTheadMouseleave);
+      theads[i].addEventListener('mouseenter', handlerTheadMouseenter);
+      theads[i].addEventListener('mouseleave', handlerTheadMouseleave);
     }
   }
 
@@ -202,15 +192,14 @@
 
   // 添加复制列悬浮事件
   function createCopyEventListener() {
-    const copy = document.querySelector('.table-wrap__copy');
-    addHandler(copy, 'mouseenter', handleCopyEnter);
-    addHandler(copy, 'mouseleave', handleCopyLeave);
+    const copy = document.querySelector('.d-copy-table__copy');
+    copy.addEventListener('mouseenter', handleCopyEnter);
+    copy.addEventListener('mouseleave', handleCopyLeave);
   }
 
   // checkbox 处理事件
   function handlerCheckBox(event) {
-    const e = getEvent(event);
-    const target = getTarget(e);
+    const target = event.target;
     const value = target.checked;
     if (value) {
       if (isFrist) {
@@ -225,48 +214,58 @@
 
   // 创建复选框 change 事件
   function createCheckBoxChangeEventListener() {
-    const checkBox = document.querySelector('.checkbox');
-    addHandler(checkBox, 'change', handlerCheckBox);
+    const checkBox = document.querySelector('.d-copy-table__checkbox');
+    checkBox.addEventListener('change', handlerCheckBox);
+  }
+
+  // 创建启动复制元素
+  function createCheckBoxElement() {
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.setAttribute('class', 'd-copy-table__checkbox');
+    return checkBox;
   }
 
   // 创建复制表格元素
   function createCopyTableElement() {
     const div = document.createElement('div');
-    const template = `<input type="text" value="1,2,3" class="table-wrap__input" />
-    <svg t="1670157694323" class="copy-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2684" width="16" height="16"><path d="M672 832 224 832c-52.928 0-96-43.072-96-96L128 160c0-52.928 43.072-96 96-96l448 0c52.928 0 96 43.072 96 96l0 576C768 788.928 724.928 832 672 832zM224 128C206.368 128 192 142.368 192 160l0 576c0 17.664 14.368 32 32 32l448 0c17.664 0 32-14.336 32-32L704 160c0-17.632-14.336-32-32-32L224 128z" p-id="2685" fill="#cdcdcd"></path><path d="M800 960 320 960c-17.664 0-32-14.304-32-32s14.336-32 32-32l480 0c17.664 0 32-14.336 32-32L832 256c0-17.664 14.304-32 32-32s32 14.336 32 32l0 608C896 916.928 852.928 960 800 960z" p-id="2686" fill="#cdcdcd"></path><path d="M544 320 288 320c-17.664 0-32-14.336-32-32s14.336-32 32-32l256 0c17.696 0 32 14.336 32 32S561.696 320 544 320z" p-id="2687" fill="#cdcdcd"></path><path d="M608 480 288.032 480c-17.664 0-32-14.336-32-32s14.336-32 32-32L608 416c17.696 0 32 14.336 32 32S625.696 480 608 480z" p-id="2688" fill="#cdcdcd"></path><path d="M608 640 288 640c-17.664 0-32-14.304-32-32s14.336-32 32-32l320 0c17.696 0 32 14.304 32 32S625.696 640 608 640z" p-id="2689" fill="#cdcdcd"></path></svg>`;
-    div.classList.add('table-wrap__copy');
+    const template = `<input type="text" value="1,2,3" class="d-copy-table__input" />
+    <svg t="1670157694323" class="d-copy-table__icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2684" width="20" height="20"><path d="M672 832 224 832c-52.928 0-96-43.072-96-96L128 160c0-52.928 43.072-96 96-96l448 0c52.928 0 96 43.072 96 96l0 576C768 788.928 724.928 832 672 832zM224 128C206.368 128 192 142.368 192 160l0 576c0 17.664 14.368 32 32 32l448 0c17.664 0 32-14.336 32-32L704 160c0-17.632-14.336-32-32-32L224 128z" p-id="2685" fill="#cdcdcd"></path><path d="M800 960 320 960c-17.664 0-32-14.304-32-32s14.336-32 32-32l480 0c17.664 0 32-14.336 32-32L832 256c0-17.664 14.304-32 32-32s32 14.336 32 32l0 608C896 916.928 852.928 960 800 960z" p-id="2686" fill="#cdcdcd"></path><path d="M544 320 288 320c-17.664 0-32-14.336-32-32s14.336-32 32-32l256 0c17.696 0 32 14.336 32 32S561.696 320 544 320z" p-id="2687" fill="#cdcdcd"></path><path d="M608 480 288.032 480c-17.664 0-32-14.336-32-32s14.336-32 32-32L608 416c17.696 0 32 14.336 32 32S625.696 480 608 480z" p-id="2688" fill="#cdcdcd"></path><path d="M608 640 288 640c-17.664 0-32-14.304-32-32s14.336-32 32-32l320 0c17.696 0 32 14.304 32 32S625.696 640 608 640z" p-id="2689" fill="#cdcdcd"></path></svg>`;
+    div.classList.add('d-copy-table__copy');
     div.innerHTML = template;
     return div;
   }
 
-  // 创建启动复制元素
-  function createCheckBox() {
-    const checkBox = document.createElement('input');
-    checkBox.setAttribute('type', 'checkbox');
-    checkBox.setAttribute('class', 'checkbox');
-    return checkBox;
+  function createEditEventListener() {
+    const edit = document.querySelector('#editPageLink');
+    const handler = function () {
+      const copy = document.querySelector('.d-copy-table__checkbox');
+      copy.style.display = 'none';
+    };
+    edit.addEventListener('click', handler);
   }
 
   // 初始元素
   function initElement() {
-    const divCopy = createCopyTableElement();
-    const checkBox = createCheckBox();
-    document.body.appendChild(divCopy);
-    document.body.appendChild(checkBox);
+    const copyElement = createCopyTableElement();
+    const checkBoxElement = createCheckBoxElement();
+    document.body.appendChild(copyElement);
+    document.body.appendChild(checkBoxElement);
   }
 
   // 加载样式
   function loadStyle() {
     const css = `
-          .table-wrap__copy{
+          .d-copy-table__copy{
             position: fixed;
             z-index: 999;
             display: none;
             padding: 5px;
             border-radius: 4px;
+            border: 1px solid #C1C7D0;
             background-color: white;
           }
-          .table-wrap__input{
+          .d-copy-table__input{
             width: 80px;
             height: 20px;
             padding: 0 8px;
@@ -274,11 +273,11 @@
             border: 1px solid #ccc;
             color: #333;
           }
-          .copy-icon{
+          .d-copy-table__icon{
             vertical-align: middle;
             cursor: pointer;
           }
-          .checkbox{
+          .d-copy-table__checkbox{
             position: fixed;
             bottom: 16px;
             right: 16px;
@@ -287,9 +286,20 @@
     GM_addStyle(css);
   }
 
-  loadStyle();
-  initElement();
-  createCopyEventListener();
-  createCheckBoxChangeEventListener();
-  createIconEventListener();
+  timer = setInterval(function () {
+    const tableWrap = document.querySelector('.table-wrap');
+    if (tableWrap) {
+      clearInterval(timer);
+      loadStyle();
+      initElement();
+      createEditEventListener();
+      createCopyEventListener();
+      createCheckBoxChangeEventListener();
+      createIconEventListener();
+    }
+  }, 500);
+
+  setTimeout(function () {
+    clearInterval(timer);
+  }, 6000);
 })();
